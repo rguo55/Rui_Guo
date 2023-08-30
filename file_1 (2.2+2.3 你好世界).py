edc80edbaa7bd4ebe5adbea0c2422a5f67369bfe
rguo55 <rguo57@gmail.com>
2023-08-30 19:01:42 -0400

    Create file_1 (2.2+2.3 你好世界).py

---
+++ file_1 (2.2+2.3 你好世界).py
@@ -0,0 +1,15 @@
+print("Dad!!!")
+print('Hello World!')
+print("python crash course" .upper())
+print("PYTHON CRASH COURSE".lower())
+
+book_name = "    python crash course"
+print(book_name.lstrip())
+
+book_name_2 = "to kill a mockingbird       "
+print(book_name_2.rstrip())
+
+book_name_3 = "        gone with the wind        "
+print(book_name_3.strip())
+
+print()
